86d936a34615d40afaab4f23947f669cc4c9f37d
Gabriel Vítor <89099674+bielvitooor@users.noreply.github.com>
2023-03-10 19:11:43 -0300

    update

---
+++ functions.js
@@ -0,0 +1,12 @@
+function typeWrite(elemento) {
+  const textoArray = elemento.innerHTML.split('');
+  elemento.innerHTML = '';
+  textoArray.forEach((letra, i) => {
+    setTimeout(() => {
+      elemento.innerHTML += letra;
+    }, 75 * i);
+  });
+}
+
+const titulo = document.querySelector('#avatarhome h1');
+typeWrite(titulo);
